243b8cb00bfff98bfe951dede96f2c67211dcdcb
Mohamed Waleed <mw99746@gmail.com>
2026-03-24 22:56:05 +0200

    feat: add migrations for floors, rooms, and reservations

---
+++ database/migrations/2026_03_24_195950_create_room_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('number')->unique();
+            $table->integer('capacity');
+            $table->bigInteger('price');
+            $table->foreignId('floor_id')->constrained('floors')->restrictOnDelete();
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('room');
+    }
+};
